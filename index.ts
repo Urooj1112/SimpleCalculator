e5bec9c2d7e508fbcf2bf9fc97c0a2a3ec750e31
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-04-18 19:34:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+const result = await inquirer.prompt([
+    {
+        Message: "Enter Your First Number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        Message: "Enter Your Second Number",
+        type: "number",
+        name: "secondNumber"
+    },
+    {
+        message: "Select Any One Operator To Perform Action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(result);
+
+if (result.operator === "Addition") {
+    console.log(result.firstNumber + result.secondNumber);
+} else if (result.operator === "Subtraction") {
+    console.log(result.firstNumber - result.secondNumber);
+} else if (result.operator === "Multiplication") {
+    console.log(result.firstNumber * result.secondNumber);
+} else if (result.operator === "Division") {
+    console.log(result.firstNumber / result.secondNumber);
+} else {
+    console.log("Select Valid Operator To Perform Your Action And Try Again");
+}
